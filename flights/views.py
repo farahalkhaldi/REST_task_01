8d5732c12e460920299c68300d5d2603acca8d55
farah alkhaldi <farahalkhaldii@outlook.com>
2019-11-24 18:41:52 +0300

    done

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,14 @@
 from .models import Flight, Booking
+from .serializers import FlightSerializer, BookingSerializer
+
+from rest_framework.generics import ListAPIView
+from datetime import datetime
+
+class FlightsList(ListAPIView):
+	queryset = Flight.objects.all()
+	serializer_class = FlightSerializer
+
+
+class BookingsList(ListAPIView):
+	queryset = Booking.objects.filter(date__gte=datetime.today())
+	serializer_class = BookingSerializer
